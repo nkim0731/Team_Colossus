25a8e75ccf4747e34ffd3b6e6e7e7ad418c87e63
Huayi Tian <89336601+HappyTTian@users.noreply.github.com>
2023-10-25 18:46:44 -0700

    add chat ui

--- Frontend/app/src/main/java/com/example/frontend/MainActivity.java
+++ Frontend/app/src/main/java/com/example/frontend/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button chatButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chatButton = findViewById(R.id.buttonChat);
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent chatIntent = new Intent(MainActivity.this,GroupChat.class);
+                startActivity(chatIntent);
+            }
+        });
     }
 }
