b0ba448a7969df9a275e059971f8eadb763eb452
Huayi Tian <89336601+HappyTTian@users.noreply.github.com>
2023-10-27 21:41:49 -0700

    add dependency to io.socket

--- Frontend/app/build.gradle.kts
+++ Frontend/app/build.gradle.kts
@@ -37,5 +37,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation ("io.socket:socket.io-client:2.1.0")
+
 
 }
