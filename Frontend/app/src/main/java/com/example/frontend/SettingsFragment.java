2ea630dc1e6f71c8ceb7067f9500a5fc0f6c8c0b
Huayi Tian <89336601+HappyTTian@users.noreply.github.com>
2023-10-30 22:24:16 -0700

    organize preferences

--- Frontend/app/src/main/java/com/example/frontend/SettingsFragment.java
+++ Frontend/app/src/main/java/com/example/frontend/SettingsFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
@@ -24,6 +25,51 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         user_email = "sample@gmail.com"; //TODO fetch from Intent
         server_url = String.format("http://10.0.2.2:3000/api/users/%s/preferences",user_email);
 
+        //Set summary provide of each editText preference
+        EditTextPreference preparation_time= findPreference("preparation_time");
+        EditTextPreference max_missed_bus= findPreference("max_missed_bus");
+        EditTextPreference snooze_duration= findPreference("snooze_duration");
+        EditTextPreference home_location= findPreference("home_location");
+        EditTextPreference school_location= findPreference("school_location");
+        EditTextPreference work_location= findPreference("work_location");
+        preparation_time.setSummaryProvider(new Preference.SummaryProvider<EditTextPreference>() {
+            @Override
+            public CharSequence provideSummary(EditTextPreference preference) {
+                return preference.getText();
+            }
+        });
+        max_missed_bus.setSummaryProvider(new Preference.SummaryProvider<EditTextPreference>() {
+            @Override
+            public CharSequence provideSummary(EditTextPreference preference) {
+                return preference.getText();
+            }
+        });
+        snooze_duration.setSummaryProvider(new Preference.SummaryProvider<EditTextPreference>() {
+            @Override
+            public CharSequence provideSummary(EditTextPreference preference) {
+                return preference.getText();
+            }
+        });
+        home_location.setSummaryProvider(new Preference.SummaryProvider<EditTextPreference>() {
+            @Override
+            public CharSequence provideSummary(EditTextPreference preference) {
+                return preference.getText();
+            }
+        });
+        school_location.setSummaryProvider(new Preference.SummaryProvider<EditTextPreference>() {
+            @Override
+            public CharSequence provideSummary(EditTextPreference preference) {
+                return preference.getText();
+            }
+        });
+        work_location.setSummaryProvider(new Preference.SummaryProvider<EditTextPreference>() {
+            @Override
+            public CharSequence provideSummary(EditTextPreference preference) {
+                return preference.getText();
+            }
+        });
+
+        //Handle preferences changes
         SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
         preferenceChangeListener = (sharedPrefs, key) -> {
             HttpsRequest putRequest = new HttpsRequest();
