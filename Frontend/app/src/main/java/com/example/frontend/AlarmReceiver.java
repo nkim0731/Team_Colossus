1b148d7b1b6204d90e29055eefcef69854fc67d3
Huayi Tian <89336601+HappyTTian@users.noreply.github.com>
2023-11-01 00:34:09 -0700

    add alarm receiver

---
+++ Frontend/app/src/main/java/com/example/frontend/AlarmReceiver.java
@@ -0,0 +1,38 @@
+package com.example.frontend;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    private final String CHANNEL_ID = "32";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Handle the alarm here. For instance, you can show a notification.
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "32")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Alarm")
+                .setContentText("sample alarm notification")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        Log.d("Alarm", "Alarm triggered");
+
+        notificationManager.notify(200, builder.build());
+    }
+}
+
