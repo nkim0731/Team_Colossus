4779a991c2ab623e1d83bb8ddc97b9bef184f3f5
Huayi Tian <89336601+HappyTTian@users.noreply.github.com>
2023-11-01 00:49:53 -0700

    add alarm manager

--- Frontend/app/src/main/java/com/example/frontend/AfterSuccessLoginActivity.java
+++ Frontend/app/src/main/java/com/example/frontend/AfterSuccessLoginActivity.java
@@ -1,11 +1,17 @@
 package com.example.frontend;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class AfterSuccessLoginActivity extends AppCompatActivity  {
     private final String TAG = "AfterSuccessLoginActivity";
@@ -41,10 +47,35 @@ public class AfterSuccessLoginActivity extends AppCompatActivity  {
         settingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.activity_preference);
+
                 // move to setting page, set preference
                 Intent settingIntent = new Intent(AfterSuccessLoginActivity.this, PreferenceActivity.class);
                 startActivity(settingIntent);
+            }
+        });
+
+        Button alarmButton = findViewById(R.id.button_alarm);
+        alarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                Intent intent = new Intent(AfterSuccessLoginActivity.this, AlarmReceiver.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(AfterSuccessLoginActivity.this, 0, intent, 0);
+                long triggerTime = System.currentTimeMillis() + (30 * 1000); 
+
+                // Set the alarm to start at a specific time.
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(System.currentTimeMillis());
+                calendar.set(Calendar.HOUR_OF_DAY, 14); 
+                calendar.set(Calendar.MINUTE, 30);
+
+                //calendar.getTimeInMillis()
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, triggerTime,
+                        1000 * 60 * 60 * 24, pendingIntent);
+
+                Log.d("Alarm", "Alarm set");
+
+
             }
         });
     }
