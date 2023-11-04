76fb53580c477d8d51f1a60fbd34484dcd9abaf9
nkim0731 <nykimsince1996@gmail.com>
2023-11-03 22:00:09 -0700

    Errors in EventDisplayAvtivity

--- Frontend/app/src/main/java/com/example/frontend/EventDisplayActivity.java
+++ Frontend/app/src/main/java/com/example/frontend/EventDisplayActivity.java
@@ -1,21 +1,50 @@
 package com.example.frontend;
 
-import android.location.Location;
-import android.location.LocationListener;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.location.LocationRequest;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class EventDisplayActivity extends AppCompatActivity implements LocationListener {
+public class EventDisplayActivity extends AppCompatActivity {
     private final String TAG = "EventDisplayActivity";
+    private final String server_url = "http://10.0.2.2:3000"; // TODO update with actual url
     EventAdapter eventAdapter;
-    List<EventData> dataArrayList = new ArrayList<>(); // replace to list of received events
+    List<EventData> dataArrayList = new ArrayList<>(); // list of recieved events
 
     private RecyclerView rv_eventList;
 
@@ -23,43 +52,198 @@ public class EventDisplayActivity extends AppCompatActivity implements LocationL
     String latLong;
     private HttpsRequest httpsRequest = new HttpsRequest();
 
+    private LocationRequest locationRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        // current location
+        locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 100)
+                .setWaitForAccurateLocation(false)
+                .setMinUpdateIntervalMillis(2000)
+                .setMaxUpdateDelayMillis(100)
+                .build();
+
+        getCurrentLocation();
+
 
         userData = getIntent().getExtras();
         String userEmail = userData.getString("userEmail", "default User Email");
         userData.putString("location", latLong);
 //        String latLong = userData.getString("", "default Latitude and Longitude");
 
+        Log.d(TAG, "Lat and Long: "+ latLong);
 
+
+
+        //initialize recyclerView
         rv_eventList = findViewById(R.id.rv_eventList);
         rv_eventList.setLayoutManager(new LinearLayoutManager(this));
         eventAdapter = new EventAdapter(dataArrayList,this);
         rv_eventList.setAdapter(eventAdapter);
 
-        // HTTP
+        // don't need upper part. just recieve an array of events from server.
         // POST : {user email, location, preference}
-        // GET : dataArrayList = events[];
+        // GET : dataArrayList = response(events[]);
+        // send necessary data to backend for database
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("username", userEmail);
+            postData.put("access_token", latLong);
+        } catch (JSONException e){
+            Log.e(TAG, "unexpected JSON exception", e);
+        }
 
-        // replace dataArrayList from received schedule event list from server.
-        EventData newEvent1 = new EventData("startTime", "eventName", "duration");
-        EventData newEvent2 = new EventData("3:30pm", "CPEN321", "1.5 hour");
-        dataArrayList.add(newEvent1);
-        dataArrayList.add(newEvent2);
+        httpsRequest.post(server_url + "/api/calendar/day_schedule", postData, new HttpsCallback() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject responseObj = new JSONObject(response);
+                    dataArrayList = responseObj.getJSONArray();
 
+                } catch (JSONException e) {
+                    Log.e(TAG, "JSON Exception on response");
+                }
+            }
+            @Override
+            public void onFailure(String error) {
+                Log.e(TAG, "Network error: Server probably closed");
+            }
+        });
+
+//        EventData newEvent1 = new EventData("startTime", "eventName", "duration");
+//        EventData newEvent2 = new EventData("3:30pm", "CPEN321", "1.5 hour");
+//        dataArrayList.add(newEvent1);
+//        dataArrayList.add(newEvent2);
 
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        double currentLatitude = location.getLatitude();
-        double currentLongitude = location.getLongitude();
-        latLong = "" + currentLatitude + ", " + currentLongitude;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        if (requestCode == 1){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
 
-        Log.d(TAG, "Lat and Long: "+ latLong);
+                if (isGPSEnabled()) {
+
+                    getCurrentLocation();
+
+                }else {
+                    turnOnGPS();
+                }
+            }
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                getCurrentLocation();
+            }
+        }
+    }
+
+    private void getCurrentLocation() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(EventDisplayActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                if (isGPSEnabled()) {
+
+                    LocationServices.getFusedLocationProviderClient(EventDisplayActivity.this)
+                            .requestLocationUpdates(locationRequest, new LocationCallback() {
+                                @Override
+                                public void onLocationResult(@NonNull LocationResult locationResult) {
+                                    super.onLocationResult(locationResult);
+
+                                    LocationServices.getFusedLocationProviderClient(EventDisplayActivity.this)
+                                            .removeLocationUpdates(this);
+
+                                    if (locationResult != null && locationResult.getLocations().size() >0){
+
+                                        int index = locationResult.getLocations().size() - 1;
+                                        double latitude = locationResult.getLocations().get(index).getLatitude();
+                                        double longitude = locationResult.getLocations().get(index).getLongitude();
+
+                                        latLong = "" + latitude + ", " + longitude;
+
+
+                                    }
+                                }
+                            }, Looper.getMainLooper());
+
+                } else {
+                    turnOnGPS();
+                }
+
+            } else {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
+
+    private void turnOnGPS() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(EventDisplayActivity.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(EventDisplayActivity.this, 2);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        });
+
+    }
+
+    private boolean isGPSEnabled() {
+        LocationManager locationManager = null;
+        boolean isEnabled = false;
+
+        if (locationManager == null) {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        }
+
+        isEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return isEnabled;
+
+    }
+
+
+
 }
