55e254e21b85850a3615918196c77e5ba96e51a6
so <sou.nozaki@gmail.com>
2023-11-04 15:54:27 -0700

    MainActiv

--- Frontend/app/src/main/java/com/example/frontend/ServerConfig.java
+++ Frontend/app/src/main/java/com/example/frontend/ServerConfig.java
@@ -1,5 +1,6 @@
 package com.example.frontend;
 
+// This class sets the server_url for every java class used
 public class ServerConfig {
     //public static final String SERVER_URL = "http://10.0.2.2:3000"; // Your default server URL
 
