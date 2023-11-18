8d37e45703e554ab609f7c5938104e8d4ab33665
cvv1007 <charlesw784@gmail.com>
2023-11-17 23:02:18 -0800

    mock database for jest

---
+++ Backend/Mocks/MockDatabase.js
@@ -0,0 +1,105 @@
+/* Mock database to simulate Database.js */
+const deepMerge = require('deepmerge');
+
+const maxMessages = 5; // update this as needed
+
+class MockDatabase {
+    constructor() {
+        this.users = {};
+        this.chats = {};
+    }
+
+    async getUser(useremail) {
+        return this.users[useremail] || null;
+    }
+    
+    async addUser(user) {
+        if (user.password === undefined) {
+            user.password = 'Register from Google'; // no user/pw login yet anyway this field is useless lol
+        }
+        // default user preferences on account creation
+        user.preferences = { 
+            commute_method: 'Driving', // default for navigation
+            preparation_time: '0',
+            notification_preferences: { // all default true
+                morning_alarm: true,
+                event_alarm: true,
+                event_notification: true,
+                traffic_alerts: true,
+                weather_alerts: true,
+            },
+            maxMissedBus: '1',
+        };
+        user.events = [];
+        user.daySchedule = [];
+
+        this.users[user.username] = user;
+        return user;
+    }
+
+    async updatePreferences(user, preferences) {
+        if (!this.users[user.username]) throw new Error("No user found");
+        this.users[user.username].preferences = deepMerge(this.users[user.username].preferences, preferences);
+
+        return this.users[user.username];
+    }
+
+	async getCalendar(username) {
+        return this.users[username].events;
+	}
+
+    async addEvents(username, events) {
+        const coursePattern = /^[A-Za-z]{4}\d{3}/;
+        let newEvents = [];
+        for (let e of events) {
+            if (coursePattern.test(e.eventName)) {
+                e.hasChat = true;
+            } else {
+                e.hasChat = false;
+            }
+            let included = false;
+            for (let ue of this.users[username].events) {
+                if (ue.eventName === e.eventName) included = true;
+            }
+            if (!included) newEvents.push(e);
+        }
+        this.users[username].events = [...this.users[username].events, ...newEvents];
+
+        return this.users[username].events;
+	}
+
+    async addSchedule(username, schedule) {
+        this.users[username].daySchedule = schedule;
+        return this.users[username].daySchedule;
+    }
+
+    async getSchedule(username) {
+        return this.users[username].daySchedule;
+    }
+
+    async getMessages(chatName) {
+        return this.chats[chatName].messages;
+    }
+
+    async createRoom(chatName) {
+        const room = { chatName, messages: [] };
+        return this.chats[chatName] = room;
+    }
+
+    async getRoom(chatName) {
+        return this.chats[chatName];
+    }
+
+    async addMessage(chatName, message) {
+        if (!this.chats[chatName]) {
+            this.chats[chatName] = { chatName, messages: [] };
+        }
+        this.chats[chatName].messages.unshift(message);
+        if (this.chats[chatName].messages.length > maxMessages) {
+            this.chats[chatName].messages.pop();
+        }
+        return this.chats[chatName].messages;
+    }
+}
+
+module.exports = new MockDatabase();
--- Backend/server.test.js
+++ Backend/server.test.js
@@ -1,3 +1,23 @@
 const server = require('./server.js');
+const request = require('supertest');
 
-// test needs to have 100% coverage
+// mock database
+const db = require('./Mocks/MockDatabase.js');
+jest.mock('./Databases/Database.js', () => require('./Mocks/MockDatabase.js'));
+
+/* tests needs to have 100% coverage */
+
+// testing login endpoint
+describe('login with google', () => {
+    test('first time login, register user', async () => {
+        let data = { username: "newuser" }
+        const res = await request(server)
+            .post('/api/endpoint')
+            .send(data)
+            .set('Accept', 'application/json');
+
+        expect(res.status).toBe(200);
+        const user = await db.getUser(data.username);
+        expect(user.password).toBe('Register from Google');
+    })
+})
